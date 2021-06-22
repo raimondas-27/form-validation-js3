a9867bb2a5e3cecbc9a33be6449d20802c71394b
raimondas-27 <raimondasvilnius@yahoo.com>
2021-06-22 08:59:19 +0300

    cleanup + main form css editing

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import MainForm from "./components/mainForm";
+
+function App() {
+  return (
+    <div className="App">
+      <MainForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mainForm.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+class MainForm extends Component {
+   state = {}
+
+   render() {
+      return (
+          <div className={"main-form"}>
+             <h1> main form </h1>
+          </div>
+      );
+   }
+}
+
+export default MainForm;
